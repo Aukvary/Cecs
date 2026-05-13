4a05c1ec66c26f4791c2b145b7d14c2209f25102
Aukvary <154656953+Aukvary@users.noreply.github.com>
2026-05-13 19:29:07 +0700

    collider fix

--- GameScripts/Components/Collider.c
+++ GameScripts/Components/Collider.c
@@ -3,7 +3,7 @@
 void game_collider_2D_reset(void* data) {
     GameCollider2D* collider = data;
 
-    collider->grid_entity
+    collider->grid_entity;
 }
 
 DT_REGISTER_COMPONENT(GameCollider2D, GAME_COLLIDER_2D)
